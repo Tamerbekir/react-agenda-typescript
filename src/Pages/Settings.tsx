11a37e630f9fa44223fffc52f5cac2df86510949
Tamerbekir <astoryofame@gmail.com>
2025-02-03 14:19:24 -0500

    Added username option to settings

--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -1,7 +1,60 @@
+import { ChangeEvent, useState } from "react";
+
 const Settings: React.FC = () => {
+
+  interface UserInfo {
+    username: '',
+  }
+
+  const [userInfo, setUserInfo] = useState<UserInfo>({
+    username: ''
+  })
+  const [openSettings, setOpenSettings] = useState(false)
+  const [submitName, setSubmitName] = useState<UserInfo[]>([])
+
+  const handleUsername = (event: ChangeEvent<HTMLInputElement>) => {
+    const {name, value } = event.target
+    setUserInfo({...userInfo, [name]: value})
+  }
+
+  const handleNameClick = () => {
+    setOpenSettings(!openSettings)
+  }
+
+  const handleSubmitName  = () => {
+    const updateName = [...submitName, userInfo]
+    setSubmitName(updateName)
+  }
+
   return (
     <div>
       <h1>Settings</h1>
+      
+      {openSettings ?
+      <>
+        <>
+          <p onClick={handleNameClick} style={{cursor: 'pointer'}}>Close</p>
+        </>
+        <input
+            name="username"
+            value={userInfo.username}
+            onChange={handleUsername} />
+            <button onClick={() => {
+                handleSubmitName()
+                setOpenSettings(false)
+              }} 
+            >Add</button>
+      </> : 
+          <>
+            <p onClick={handleNameClick} style={{cursor: 'pointer'}}>Change Name</p>
+          </>
+            }
+  {/* <div>
+    {submitName.map((name, index) => (
+      <div key={index}>{name.username}</div>
+    ))}
+  </div> */}
+  <p>Name: {userInfo.username}</p>
     </div>
   );
 };
